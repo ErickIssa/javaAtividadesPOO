03611d6989fdbc1436192b3f99a2f06e68dc07a2
ErickIssa <erickvinicius19675@gmail.com>
2025-09-05 16:56:11 -0300

    calc Inss

--- atividade 2/trabalho2/src/App.java
+++ atividade 2/trabalho2/src/App.java
@@ -4,9 +4,13 @@ public class App {
     public static void main(String[] args) throws Exception {
       Funcionario f1 = new Funcionario("Erick", "19/06/2006");
       f1.setTipoContrato("Horista");
-      f1.setValorSal(20);
+      f1.setValorSal(200);
+
+      int horasTra = 10;
       System.out.println(f1.getTipoContrato());
-      System.out.println(f1.calcSalario(10));
+      System.out.println(f1.calcSalarioBruto(horasTra));
+      System.out.println(f1.getSalarioDescontado(horasTra));
+      System.out.println(f1.calcularDescontoINSS(f1.calcSalarioBruto(horasTra)));
 
       Data dataSy = new Data();
       dataSy.printData();
--- atividade 2/trabalho2/src/Funcionario.java
+++ atividade 2/trabalho2/src/Funcionario.java
@@ -13,27 +13,75 @@ public class Funcionario {
     }
 
     
-    public float calcSalario(float horasTrabalhadas){
+    public float calcSalarioBruto(float horasTrabalhadas){
         float valorF;
 
-        if(this.tipoContrato == "Horista"){
+        // Corrigido: usar equals() para comparar strings
+        if("Horista".equals(this.tipoContrato)){
             valorF = this.valorSal * horasTrabalhadas;
-        }else{
-            valorF = this.valorSal;
+        } else {
+            valorF = this.valorSal; // Para outros tipos de contrato, retorna o salário fixo
         }
         return valorF;
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public double calcularDescontoINSS(float salarioContribuicao) {
+        if (salarioContribuicao <= 1302.00) {
+            return salarioContribuicao * 0.075; // 7.5%
+        } else if (salarioContribuicao <= 2571.29) {
+            return (1302.00 * 0.075) + ((salarioContribuicao - 1302.00) * 0.09);
+        } else if (salarioContribuicao <= 3856.94) {
+            return (1302.00 * 0.075) + 
+                   ((2571.29 - 1302.00) * 0.09) + 
+                   ((salarioContribuicao - 2571.29) * 0.12);
+        } else if (salarioContribuicao <= 7507.49) {
+            return (1302.00 * 0.075) + 
+                   ((2571.29 - 1302.00) * 0.09) + 
+                   ((3856.94 - 2571.29) * 0.12) + 
+                   ((salarioContribuicao - 3856.94) * 0.14);
+        } else {
+            // Para salários acima do teto, calcula apenas até o limite máximo
+            return (1302.00 * 0.075) + 
+                   ((2571.29 - 1302.00) * 0.09) + 
+                   ((3856.94 - 2571.29) * 0.12) + 
+                   ((7507.49 - 3856.94) * 0.14);
+        }
+    }
+
+    public double calcularIRRF(double salario) {
+    int dependentes = this.nmrFilhos;
+    double deducaoDependentes = dependentes * 189.59;
+
+    // Base de cálculo
+    double baseCalculo = salario - deducaoDependentes;
+
+    if (baseCalculo <= 1903.98) {
+        return 0.0;
+    } else if (baseCalculo <= 2826.65) {
+        return (baseCalculo - 1903.98) * 0.075;
+    } else if (baseCalculo <= 3751.05) {
+        return (2826.65 - 1903.98) * 0.075 +
+               (baseCalculo - 2826.65) * 0.15;
+    } else if (baseCalculo <= 4664.68) {
+        return (2826.65 - 1903.98) * 0.075 +
+               (3751.05 - 2826.65) * 0.15 +
+               (baseCalculo - 3751.05) * 0.225;
+    } else {
+        return (2826.65 - 1903.98) * 0.075 +
+               (3751.05 - 2826.65) * 0.15 +
+               (4664.68 - 3751.05) * 0.225 +
+               (baseCalculo - 4664.68) * 0.275;
+    }
+}
+
+
+    public float getSalarioDescontado(int horasTrabalhadas) {
+        float salarioBruto = calcSalarioBruto(horasTrabalhadas);
+        double descontoINSS = calcularDescontoINSS(salarioBruto);
+        return (float) (salarioBruto - descontoINSS);
+    }
+            
     
     //--------------------------------------------------------------------------------
     public String getTipoContrato(){
